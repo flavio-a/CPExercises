70cb9aeb7e74c43031d74c4dff8d4019efa10275
Flavio <flavio.a.618@gmail.com>
2018-10-02 15:48:28 +0200

    Added empty files for lecture5

---
+++ lecture5/Alice_Bob_and_chocolate.cpp
@@ -0,0 +1,20 @@
+// http://codeforces.com/problemset/problem/6/C?locale=en
+// TODO
+/*
+
+
+
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	int T;
+	cin >> T;
+	for (int t = 0; t < T; ++t) {
+
+	}
+	return 0;
+}
---
+++ lecture5/Update_the_array.cpp
@@ -0,0 +1,34 @@
+// https://www.spoj.com/problems/UPDATEIT/
+// TODO
+/*
+
+The Fenwick tree allows for range queries and point update. To allow point
+queries and range updates I simply create the Fenwick tree on the difference
+array.
+
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Returns the element at index i of the array, that is the sum from 0 to i
+// in the difference array, that is the query on the Fenwick tree
+void getVal(vector<int> v, int i) {
+
+}
+
+// Adds x to all elements from i (included) onward, that is the edit of i in the
+// difference array, that is the update on the Fenwick tree
+void addVal(vector<int> v, int i, int x) {
+
+}
+
+int main() {
+	int T;
+	cin >> T;
+	for (int t = 0; t < T; ++t) {
+
+	}
+	return 0;
+}
