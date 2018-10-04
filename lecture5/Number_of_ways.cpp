a5ab74ed4f06c698d18bb56339bc5c003ca8aa9c
Flavio <flavio.a.618@gmail.com>
2018-10-04 11:47:26 +0200

    Faster solution for lecture5/number_of_ways

--- lecture5/Number_of_ways.cpp
+++ lecture5/Number_of_ways.cpp
@@ -3,38 +3,18 @@
 
 Computes a third of the total sum. If it isn't integer the answer is 0, else
 computes an array with 1 if the prefix sum up to that index is equal to a third
-of the total, else 0. Stores this array in a Fenwick, and uses it to computes
-the number of indexes with prefix sum equal to a third lower than a certain
-index. Now iterating over indexes from the end and computing suffix sums get
-the job done.
+of the total, else 0. In place of this array, it stores its array of prefix
+sums, to allow queries in constant time. Now iterating over indexes from the end
+and computing suffix sums get the job done.
 
-Space complexity is Theta(n) for the Fenwick tree. Time complexity is Theta(n)
-to initialize the Fenwick, O(n log(n)) to fill the Fenwick and again O(n log(n))
-when computing suffix sums and querying the Fenwick.
+Space complexity is Theta(n) for the prefix sums. Time complexity is Theta(n)
+for each single operation, thus the total is Theta(n).
 
 */
 
 #include <bits/stdc++.h>
 
 using namespace std;
-
-#define lsb(x) (x & -x)
-
-void addVal(vector<int>& v, int i, int x) {
-	while (i < v.size()) {
-		v[i] += x;
-		i += lsb(i);
-	}
-}
-
-int getSum(const vector<int>& v, int i) {
-	int result = 0;
-	while (i > 0) {
-		result += v[i];
-		i -= lsb(i);
-	}
-	return result;
-}
 
 int main() {
 	int n;
@@ -52,13 +32,14 @@ int main() {
 		return 0;
 	}
 	int thirdsum = sum / 3;
-	// Fills the Fenwick tree
-	vector<int> ftree(n + 1, 0);
+	// Fills the prefix
+	int prefs[n] = {0};
 	int prefixsum = 0;
 	for (int i = 0; i < n; ++i) {
 		prefixsum += A[i];
+		prefs[i] = prefs[i - 1];
 		if (prefixsum == thirdsum)
-			addVal(ftree, i + 1, 1);
+			prefs[i]++;
 	}
 
 	int result = 0;
@@ -66,7 +47,7 @@ int main() {
 	for (int i = n - 1; i >= 0; --i) {
 		suffixsum += A[i];
 		if (suffixsum == thirdsum)
-			result += getSum(ftree, (i - 2) + 1);
+			result += prefs[i - 2];
 	}
 	cout << result << endl;
 	return 0;
