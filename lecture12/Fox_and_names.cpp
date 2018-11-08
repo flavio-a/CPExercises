0b282a71d8a0edc193a40e7dc67baeefae2cc9cf
Flavio <flavio.a.618@gmail.com>
2018-11-08 15:55:45 +0100

    Almost working solution

--- lecture12/Fox_and_names.cpp
+++ lecture12/Fox_and_names.cpp
@@ -16,6 +16,29 @@ using namespace std;
 
 // Maps lowercase letters to [0; 26]
 #define num(l) (l - 'a')
+#define letter(n) (char)(n + 'a')
+
+// Returns false iff it found a cycle
+bool recursiveDFS(const bool G[26][26], char v, vector<char> &colors, stack<char> &order) {
+	colors[v] = 1;
+	// Visit neighbours
+	for (int i = 0; i < 26; ++i) {
+		if (G[v][i]) {
+			// i is a neighbour
+			if (colors[i] == 1)
+				// v is a descendant of i, cycle
+				return false;
+			if (colors[i] == 0) {
+				if (!recursiveDFS(G, i, colors, order))
+					return false;
+			}
+		}
+	}
+	colors[v] = 2;
+	// cout << "Pushing " << letter(v) << "\n";
+	order.push(v);
+	return true;
+}
 
 int main() {
 	int N;
@@ -29,20 +52,31 @@ int main() {
 
 	for (int i = 1; i < N; ++i) {
 		int j = 0;
-		while (words[i - 1][j] == words[i][j])
+		while (j < words[i - 1].length() && j < words[i].length() && words[i - 1][j] == words[i][j])
 			++j;
-		if (words[i - 1][j] < words[i][j]) {
+		if (j < words[i - 1].length() && j < words[i].length())
 			// Add words[i - 1][j] -> words[i][j]
-			G[num(words[i - 1][j])][words[i][j]] = true;
-		}
-		else {
-			// Add words[i - 1][j] <- words[i][j]
-			G[num(words[i][j])][words[i - 1][j]] = true;
+			G[num(words[i - 1][j])][num(words[i][j])] = true;
+	}
+
+	stack<char> order;
+	vector<char> colors;
+	colors.resize(26, 0);
+
+	// Searches for topological sort, checking also for acyclicness
+	for (int v = 0; v < 26; ++v) {
+		if (colors[v] == 0) {
+			if (!recursiveDFS(G, v, colors, order)) {
+				cout << "Impossible" << endl;
+				return 0;
+			}
 		}
 	}
 
-	// Searches for topological sort, checking also for acyclicness
-	
+	while (!order.empty()) {
+		cout << letter(order.top());
+		order.pop();
+	}
 
 	return 0;
 }
