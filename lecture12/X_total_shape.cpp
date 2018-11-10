a30a8c085e757346b90cafee85cd4c7df53a6f96
Flavio <flavio.a.618@gmail.com>
2018-11-10 11:44:16 +0100

    Solved X_total_shape and checkposts

--- lecture12/X_total_shape.cpp
+++ lecture12/X_total_shape.cpp
@@ -1,8 +1,10 @@
 // https://practice.geeksforgeeks.org/problems/x-total-shapes/0
-// TODO
 /*
 
+Plain DFS to count connected components on the graph of the Xs.
 
+Time and space complexity are both O(N * M) for the DFS and to store the graph,
+respectively.
 
 */
 
@@ -10,11 +12,62 @@
 
 using namespace std;
 
-int main() {
-	int N;
-	cin >> N;
-	for (int i = 0; i < N; ++i) {
+void DFS(const vector<vector<int>> &G, int x, vector<bool> &visited) {
+	visited[x] = true;
+	for (auto u = G[x].begin(); u != G[x].end(); ++u)
+		if (!visited[*u])
+			DFS(G, *u, visited);
+}
 
+int main() {
+	int T;
+	cin >> T;
+	for (int t = 0; t < T; ++t) {
+		int N, M;
+		cin >> N >> M;
+		vector<vector<int>> G;
+		G.resize(N * M);
+		vector<int> Xs;
+		Xs.reserve(N * M);
+		char line[M];
+		for (int i = 0; i < N; ++i) {
+			for (int j = 0; j < M; ++j) {
+				char c;
+				cin >> c;
+				if (c == 'X') {
+					Xs.push_back(i * M + j);
+					if (j > 0 && line[j - 1] == 'X') {
+						G[i * M + j].push_back(i * M + j - 1);
+						G[i * M + j - 1].push_back(i * M + j);
+					}
+					if (i > 0 && line[j] == 'X') {
+						G[i * M + j].push_back((i - 1) * M + j);
+						G[(i - 1) * M + j].push_back(i * M + j);
+					}
+				}
+				line[j] = c;
+			}
+		}
+
+		// for (int i = 0; i < N * M; ++i) {
+		// 	cout << "(" << (i / M) << ", " << (i % M) << ") -> ";
+		// 	for (auto p = G[i].begin(); p != G[i].end(); ++p) {
+		// 		cout << "(" << (*p / M) << ", " << (*p % M) << ") ";
+		// 	}
+		// 	cout << "\n";
+		// }
+
+		int cc = 0;
+		vector<bool> visited;
+		visited.resize(N * M, false);
+		for (auto x = Xs.begin(); x != Xs.end(); ++x) {
+			if (!visited[*x]) {
+				++cc;
+				DFS(G, *x, visited);
+			}
+		}
+
+		cout << cc << endl;
 	}
 	return 0;
 }
--- lecture13/Checkposts.cpp
+++ lecture13/Checkposts.cpp
@@ -1,8 +1,17 @@
 // http://codeforces.com/problemset/problem/427/C?locale=en
-// TODO
 /*
 
+A checkpost secures all and only junctions in its strongly connected components:
+a junction is secured iff a patrol can go to that junction and come back to the
+checkpost iff the checkpost and the junction are in the same SCC.
 
+Thus the problem is to compute the minimum cost of a junction in each SCC, that
+is the sum over the SCC of the minimum cost a junction in that SCC. The number
+of ways is the product over the SCC of the number of junctions with that minimum
+cost.
+
+Time complexity is O(V + E) = O(n + m) to compute the SCC (via DFS). Space
+complexity is O(V + E) = O(n + m) to store the graph.
 
 */
 
@@ -10,11 +19,96 @@
 
 using namespace std;
 
+#define lli long long int
+
+void DFS(const vector<vector<unsigned int>> &G, int v, vector<bool> &visited, stack<unsigned int> &f) {
+	visited[v] = true;
+	for (auto u = G[v].begin(); u != G[v].end(); ++u)
+		if (!visited[*u])
+			DFS(G, *u, visited, f);
+	f.push(v);
+}
+
+// During the second DFS each call from the main loop explore exactly a SCC,
+// so it's possible to do now any computation needed
+void DFS_SCC(const vector<vector<unsigned int>> &G, int v, vector<bool> &visited, const vector<unsigned int> &costs, stack<unsigned int> &mincost, stack<unsigned int> &number) {
+	// cout << v << " ";
+	visited[v] = true;
+	if (costs[v] == mincost.top())
+		++number.top();
+	if (costs[v] < mincost.top()) {
+		mincost.top() = costs[v];
+		number.top() = 1;
+	}
+	for (auto u = G[v].begin(); u != G[v].end(); ++u)
+		if (!visited[*u])
+			DFS_SCC(G, *u, visited, costs, mincost, number);
+}
+
 int main() {
 	int N;
 	cin >> N;
-	for (int i = 0; i < N; ++i) {
-
+	vector<unsigned int> costs;
+	costs.resize(N);
+	for (int i = 0; i < N; ++i)
+		cin >> costs[i];
+	vector<vector<unsigned int>> G;
+	G.resize(N);
+	vector<vector<unsigned int>> GT;
+	GT.resize(N);
+	int M;
+	cin >> M;
+	for (int i = 0; i < M; ++i) {
+		int u, v;
+		cin >> u >> v;
+		G[u - 1].push_back(v - 1);
+		GT[v - 1].push_back(u - 1);
 	}
+
+	// cout << "Printing graph:\n";
+	// for (int i = 0; i < N + M; ++i) {
+	// 	cout << i << ": ";
+	// 	for (auto j = G[i].begin(); j != G[i].end(); ++j)
+	// 		cout << *j << " ";
+	// 	cout << "\n";
+	// }
+
+	vector<bool> visited;
+	visited.resize(N, false);
+	stack<unsigned int> f;
+
+	// Sort by finish time
+	for (int i = 0; i < N; ++i)
+		if (!visited[i])
+			DFS(G, i, visited, f);
+
+	visited.clear();
+	visited.resize(N, false);
+	stack<unsigned int> mincost;
+	stack<unsigned int> number;
+	while (!f.empty()) {
+		unsigned int v = f.top();
+		if (!visited[v]) {
+			mincost.push(UINT_MAX);
+			number.push(0);
+			// cout << v << "'s SSC contains ";
+			DFS_SCC(GT, v, visited, costs, mincost, number);
+			// cout << "and has mincost = " << mincost.top() << " and number = " << number.top() << "\n";
+		}
+		f.pop();
+	}
+
+	unsigned lli totmincost = 0;
+	unsigned lli totnumber = 1;
+	while (!mincost.empty()) {
+		totmincost += (unsigned lli)mincost.top();
+		totnumber = (totnumber * (unsigned lli)number.top()) % 1000000007;
+
+		mincost.pop();
+		number.pop();
+	}
+
+	cout << totmincost << " " << totnumber << endl;
+
 	return 0;
 }
