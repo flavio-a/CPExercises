75f80798d70e18e0f71d0f7ff5545bd967f2763c
Flavio <flavio.a.618@gmail.com>
2018-12-05 17:03:54 +0100

    Working but time limit exceeding Mo's algorithm solution for Tree_and_queries

--- lecture10/Tree_and_queries.cpp
+++ lecture10/Tree_and_queries.cpp
@@ -10,112 +10,100 @@ Easy (with respect to the N * log N one) solution with Mo's algorithm.
 
 using namespace std;
 
-int linearizer(const vector<vector<int>> &G, const vector<int> &oldcolors, int u, int idx, vector<int> &newcolors, vector<pair<int, int>> &boundaries) {
-	boundaries[u] = pair<int, int>(idx, 0);
-	newcolors[u] = oldcolors[u];
+#define maxC 100001
+
+int linearize(vector<vector<int>> &G, int root, const vector<int> &colors, int idx, vector<int> &start, vector<int> &end, vector<int> &newcolors) {
+	start[root] = idx;
+	newcolors[idx] = colors[root];
 	++idx;
-	for (auto v = G[u].begin(); v != G[u].end(); ++v)
-		if (newcolors[*v] != oldcolors[*v])
-			idx = linearizer(G, oldcolors, *v, idx, newcolors, boundaries);
-	// The subarray corresponding to the subtree is [first; second)
-	boundaries[u].second = idx;
+	for (auto v : G[root]) {
+		G[v].erase(find(G[v].begin(), G[v].end(), root));
+		idx = linearize(G, v, colors, idx, start, end, newcolors);
+	}
+	end[root] = idx;
 	return idx;
 }
 
 int main() {
 	int N, M;
 	cin >> N >> M;
-	vector<int> colors;
-	colors.reserve(N);
+	vector<int> colors(N);
 	for (int i = 0; i < N; ++i) {
-		int tmp;
-		cin >> tmp;
-		colors.push_back(tmp - 1);
+		cin >> colors[i];
 	}
-	vector<vector<int>> G;
-	G.resize(N);
+
+	vector<vector<int>> G(N);
 	for (int i = 0; i < N - 1; ++i) {
-		int u, v;
-		cin >> u >> v;
-		G[u - 1].push_back(v - 1);
-		G[v - 1].push_back(u - 1);
+		int a, b;
+		cin >> a >> b;
+		--a;
+		--b;
+		G[a].push_back(b);
+		G[b].push_back(a);
 	}
+	vector<int> start(N), end(N), newcolors(N);
+	linearize(G, 0, colors, 0, start, end, newcolors);
 
-	// Linearize the tree with a preorder visit
-	vector<int> newcolors;
-	newcolors.resize(N, -1);
-	vector<pair<int, int>> boundaries;
-	boundaries.resize(N);
-	linearizer(G, colors, 0, 0, newcolors, boundaries);
-	// for (int i = 0; i < N; ++i)
-	// 	cout << "Subtree of " << i << " is [" << boundaries[i].first << ", " << boundaries[i].second << "), color " << newcolors[i] << "\n";
+	// for (int i = 0; i < N; ++i) {
+	// 	cout << "Node " << i << ": [" << start[i] << "; " << end[i] << "), color " << newcolors[i] << "\n";
+	// }
 
-	vector<pair<int, int>> queries;
-	for (int i = 0; i < M; ++i) {
-		int v, k;
-		cin >> v >> k;
-		queries.push_back(pair<int, int>(v - 1, k));
+	vector<int> v(M), k(M), qidx(M);
+	for (int j = 0; j < M; ++j) {
+		cin >> v[j] >> k[j];
+		--v[j];
+		qidx[j] = j;
 	}
-
-	int Qindexes[M];
-	iota(Qindexes, Qindexes + M, 0);
-	int sqrtN = ceil(sqrt(N));
-	// Sort queries by (bucket; end)
-	sort(Qindexes, Qindexes + M, [sqrtN, N, &queries, &boundaries](int i1, int i2) {
-		auto p1 = boundaries[queries[i1].first], p2 = boundaries[queries[i2].first];
-		if (p1.first / sqrtN == p2.first / sqrtN)
-			return p1.second < p2.second;
-		// Given that the two elements are in distinct buckets, it's enough to
-		// compare the values of l
-		return p1.first < p2.first;
+	const int sqrtM = floor(sqrt(M));
+	sort(qidx.begin(), qidx.end(), [&](int a, int b) {
+		if (start[a] / sqrtM != start[b] / sqrtM) {
+			return start[a] / sqrtM < start[b] / sqrtM;
+		}
+		else {
+			return end[a] < end[b];
+		}
 	});
 
-
-	#define MAX_COLOR 100001
-	int occ[MAX_COLOR] = {0};
-	// there are k_occ[i] colors that appears at least i times
-	int k_occ[MAX_COLOR] = {0};
-	vector<int> results;
-	results.resize(M);
-	int l = 0, r = 0;
-	for (int i = 0; i < M; ++i) {
-		// Query Qindexes[i]
-		auto borders = boundaries[queries[Qindexes[i]].first];
-		cout << "Query " << i << " -> " << Qindexes[i] << endl;
-		cout << queries[Qindexes[i]].second << " [" << borders.first << ", " << borders.second << ")\n";
-		while (r > borders.second) {
-			// cout << "-r " << r << " " << newcolors[r] << endl;
-			k_occ[occ[newcolors[r]]]--;
-			occ[newcolors[r]]--;
-			--r;
+	vector<int> results(M);
+	int s = 0, e = 0;
+	vector<int> totfreq(maxC, 0), col2freq(maxC, 0);
+	auto add = [&totfreq, &col2freq](int c){
+		++col2freq[c];
+		++totfreq[col2freq[c]];
+	};
+	auto rem = [&totfreq, &col2freq](int c){
+		--totfreq[col2freq[c]];
+		--col2freq[c];
+	};
+	for (int j = 0; j < M; ++j) {
+		// query qidx[j]
+		int sIdx = qidx[j];
+		while (s < start[v[sIdx]]) {
+			rem(newcolors[s]);
+			++s;
 		}
-		while (r < borders.second) {
-			// cout << "+r " << r << " " << newcolors[r] << endl;
-			occ[newcolors[r]]++;
-			k_occ[occ[newcolors[r]]]++;
-			++r;
+		while (s > start[v[sIdx]]) {
+			--s;
+			add(newcolors[s]);
 		}
-		while (l < borders.first) {
-			// cout << "+l " << l << " " << newcolors[l] << endl;
-			k_occ[occ[newcolors[l]]]--;
-			occ[newcolors[l]]--;
-			++l;
+		while (e < end[v[sIdx]]) {
+			add(newcolors[e]);
+			++e;
 		}
-		while (l > borders.first) {
-			--l;
-			// cout << "-l " << l << " " << newcolors[l] << endl;
-			occ[newcolors[l]]++;
-			k_occ[occ[newcolors[l]]]++;
+		while (e > end[v[sIdx]]) {
+			--e;
+			rem(newcolors[e]);
 		}
-		// for (int i = 0; i < 5; ++i)
-		// 	cout << "Color " << i << ": " << occ[i] << endl;
-		// for (int i = 1; i < 5; ++i)
-		// 	cout << "k_occ " << i << ": " << k_occ[i] << endl;
-		results[Qindexes[i]] = k_occ[queries[Qindexes[i]].second];
+		// cout << "Query " << sIdx << ": [" << s << "; " << e << ")\n";
+		// for (int i = 1; i < 4; ++i) {
+		// 	cout << "col2freq[" << i << "] = " << col2freq[i] << "\n";
+		// }
+		results[sIdx] = totfreq[k[sIdx]];
 	}
 
-	for (int i = 0; i < M; ++i)
-		cout << results[i] << "\n";
+	for (int j = 0; j < M; ++j) {
+		cout << results[j] << "\n";
+	}
 
 	return 0;
 }
